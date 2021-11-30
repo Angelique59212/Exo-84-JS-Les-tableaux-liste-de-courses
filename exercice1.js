339d1ccb6f61b964bce311416e287154e10b953c
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-30 11:54:57 +0100

    js

--- exercice1.js
+++ exercice1.js
@@ -12,6 +12,18 @@ let articles = [
 
 // Votre code à partir d'ici :
 
+let liste = document.getElementById("listeCourse");
+for (let i = 0; i < articles.length; i++ ) {
+    let elements = document.createElement("li");
+    elements.innerHTML = articles[i];
+    liste.appendChild(elements);
+    let button = document.createElement("button");
+    button.style.border = "2px solid black";
+    button.addEventListener("click", function () {
+        elements.style.display = "hidden";
+    });
+    elements.appendChild(button);
+}
 
 
 
